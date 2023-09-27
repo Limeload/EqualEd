669e9673c536e6021019c463f7eb9c97e4f3f566
Donte Ladatto <donte279@gmail.com>
2023-09-27 13:55:01 -0500

    copy over base components, add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import Login from './components/Login';
+import Courses from './components/Courses';
+import Course from './components/Course';
+import MyCourses from './components/MyCourses';
+import NewCourse from './components/NewCourse';
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login/student" element={<Login />} />
+        <Route path="/login/instructor" element={<Login />} />
+        <Route path="/my-courses" element={<MyCourses />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/courses/:courseId" element={<Course />} />
+        <Route path="/courses/new" element={<NewCourse />} />
+      </Routes>
     </div>
   );
 }
---
+++ client/src/components/NewCourse.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewCourse = () => {
+  return (
+    <div>NewCourse</div>
+  )
+}
+
+export default NewCourse
