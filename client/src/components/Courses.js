d26c7e351d3f21d5b93889e6e3d5b70e93b1a176
Donte Ladatto <donte279@gmail.com>
2023-09-28 18:42:27 -0500

    grab courses from state

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Courses = () => {
+  const courses = useSelector(state => state.courses.entities)
   return (
     <div>Courses</div>
   )
