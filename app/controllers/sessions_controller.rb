755dbd194fb4ac1bdb707f1e98aca60e6b2246d3
Shraddha Rao <shraddharaom@gmail.com>
2023-09-29 13:02:38 -0500

    merged conflicts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   skip_before_action :authorize, only: :create
 
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: "Invalid Username or Password" }, status: :unauthorized
+        end
+    end
+
+    def destroy
+        user = User.find_by(id: session[:user_id])
+        session.delete :user_id
+        head :no_content, status: :no_content
+    end
+
 end
