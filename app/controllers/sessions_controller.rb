0d8ca0b0ceccfec3bd5ba58d7e3016d0b8e31aa9
alex lieberman <alex.lieberman182@gmail.com>
2023-09-29 09:21:24 -0700

    added routes and sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: "Invalid Username or Password" }, status: :unauthorized
+        end
+    end
+
+    def destroy
+        user = User.find_by(id: session[:user_id])
+        session.delete :user_id
+        head :no_content, status: :no_content
+    end
+end
