2583bfae7dbaae22aba623af0b8773c3f59e43c5
Donte Ladatto <donte279@gmail.com>
2023-09-30 08:48:27 -0500

    navigate & render course after CourseCard click

--- client/src/components/Course.js
+++ client/src/components/Course.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
 
 const Course = () => {
+  let { courseId } = useParams()
+  const course = useSelector(state => state.courses.entities.find(c => c.id === courseId))
   return (
-    <div>Course</div>
+    <div>
+      <h2>{course.title}</h2>
+      <p>{course.content}</p>
+      {/* enroll button that dispatches enrollCourse action */}
+    </div>
   )
 }
 
--- client/src/components/CourseCard.js
+++ client/src/components/CourseCard.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const CourseCard = ({ course }) => {
+  const navigate = useNavigate()
   return (
-    <div>
+    <div onClick={() => navigate(`/courses/${course.id}`)}>
       <h3>{course.title}</h3>
+      {/* shorten the descriptions with css - just want to make the cards uniform and relatively square */}
       <p>{course.description}</p>
     </div>
   )
