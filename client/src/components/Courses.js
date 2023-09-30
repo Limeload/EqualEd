4dc90af0545fd6a9fb95c74800b54731f95389a7
Donte Ladatto <donte279@gmail.com>
2023-09-30 09:21:27 -0500

    additional course rendering groundwork

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -5,6 +5,11 @@ import { useSelector } from 'react-redux'
 const Courses = () => {
   const courses = useSelector(state => state.courses.entities)
   const displayCourses = courses.map(c => <CourseCard key={c.id} course={c} />)
+  if (!courses[0]) {
+    return (
+      <h2>There are no courses yet!</h2>
+    )
+  }
   return (
     <div>
       {displayCourses}
--- client/src/components/MyCourses.js
+++ client/src/components/MyCourses.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const MyCourses = () => {
+  const user = useSelector(state => state.sessions.currentUser)
+
+  // TODO: functions to display course collections
   return (
-    <div>MyCourses</div>
+    <div>
+      <div className="courseGrid">
+        <h2>Enrolled Courses</h2>
+      </div>
+      {user.instructor ?
+        <div className="courseGrid">
+          <h2>Owned Courses</h2>
+        </div> :
+        null
+      }
+    </div>
   )
 }
 
