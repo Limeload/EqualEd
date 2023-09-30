0c5408989b49d45f19d023622943661b86478e13
Donte Ladatto <donte279@gmail.com>
2023-09-30 11:59:40 -0500

    create EditProfile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import EditProfile from './EditProfile'
+import MyCourses from './MyCourses'
+
+// ** This page will essentially act as the dashboard for a signed in user, with their profile info and course collections displayed
+// ** Might just make a Dashboard components though, so I can toggle betweenProfile and EditProfile components
 
 const Profile = () => {
   return (
