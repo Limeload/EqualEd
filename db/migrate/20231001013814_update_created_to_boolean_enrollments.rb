bbb71c84075ab4b160430eab1158d2a372a54541
alex lieberman <alex.lieberman182@gmail.com>
2023-09-30 19:25:26 -0700

    Updated seeds

--- db/migrate/20231001013814_update_created_to_boolean_enrollments.rb
+++
@@ -1,5 +0,0 @@
-class UpdateCreatedToBooleanEnrollments < ActiveRecord::Migration[6.1]
-  def change
-    change_column :enrollments, :created, :boolean
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,54 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# puts 'Seeding students'
+# 10.times do User.create(username: Faker::GreekPhilosophers.name, email: Faker::Internet.email, instructor: false) end
 
-puts 'Seeding users'
-# user1 = User.create(
-#   username: 'user1',
-#   email: 'user1@example.com',
-#   user_type: 'Student',
-#   password: 'password123',
-#   password_confirmation: 'password123'
-# )
+# puts 'Seeding Teachers'
+# 5.times do User.create(username: Faker::GreekPhilosophers.name, email: Faker::Internet.email, instructor: true) end
 
-# user2 = User.create(
-#   username: 'user2',
-#   email: 'user2@example.com',
-#   user_type: 'Educator',
-#   password: 'password123',
-#   password_confirmation: 'password123'
-# )
+# puts 'seeding courses'
 
-user3 = User.create(
-  username: 'user3',
-  email: 'user3@example.com',
-  user_type: 'Educator',
-  instructor: true,
-  password: 'password123',
-  password_confirmation: 'password123'
-)
+# 50.times do Course.create(title: Faker::Educator.course_name, content: Faker::Books::Lovecraft.paragraph_by_chars(characters: 500)) end
 
-puts 'seeding courses'
+# puts 'seeding enrollments'
 
-# course1 = Course.create(
-#   title: 'Introduction to Algebra',
-#   content: 'This course provides a comprehensive introduction to algebra concepts.'
-# )
+# 30.times do Enrollment.create(course_id: rand(1..50), user_id: rand(1..15), enrolled: true, created: false) end
 
-# course2 = Course.create(
-#   title: 'History of Ancient Civilizations',
-#   content: 'Explore the history of ancient civilizations from around the world.'
-# )
+# 30.times do Enrollment.create(course_id: rand(1..50), user_id: rand(1..15), enrolled: false, created: true) end
 
-puts 'seeding enrollments'
-
-# e1 = Enrollment.create(user_id: 1, course_id: 1, enrolled: true)
-# e2 = Enrollment.create(user_id: 1, course_id: 2, enrolled: true)
-# e3 = Enrollment.create(user_id: 2, course_id: 2, enrolled: true)
-e4 = Enrollment.create(user_id: 2, course_id: 2, enrolled: false)
-
-puts 'seeded'
+# puts 'seeded'
