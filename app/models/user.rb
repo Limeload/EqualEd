3bd1ae6afaed9d1980096492252845a0ef232e15
alex lieberman <alex.lieberman182@gmail.com>
2023-09-30 20:05:03 -0700

    More seed stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,6 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
-  # validates :password, presence: true, length: { minimum: 8 }, on: :create
-  # validates :password, confirmation: true
-  validates :user_type, presence: true, inclusion: { in: %w(Student Educator) }
+  validates :password, presence: true, length: { minimum: 8 }, on: :create
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-# puts 'Seeding students'
-# 10.times do User.create(username: Faker::GreekPhilosophers.name, email: Faker::Internet.email, instructor: false) end
+puts 'Seeding students'
+10.times do User.create(username: Faker::GreekPhilosophers.name, email: Faker::Internet.email, instructor: false, password: hotdogs123) end
 
-# puts 'Seeding Teachers'
-# 5.times do User.create(username: Faker::GreekPhilosophers.name, email: Faker::Internet.email, instructor: true) end
+puts 'Seeding Teachers'
+5.times do User.create(username: Faker::GreekPhilosophers.name, email: Faker::Internet.email, instructor: true, password: hotdogs123) end
 
-# puts 'seeding courses'
+puts 'seeding courses'
 
-# 50.times do Course.create(title: Faker::Educator.course_name, content: Faker::Books::Lovecraft.paragraph_by_chars(characters: 500)) end
+50.times do Course.create(title: Faker::Educator.course_name, content: Faker::Books::Lovecraft.paragraph_by_chars(characters: 500)) end
 
-# puts 'seeding enrollments'
+puts 'seeding enrollments'
 
-# 30.times do Enrollment.create(course_id: rand(1..50), user_id: rand(1..15), enrolled: true, created: false) end
+30.times do Enrollment.create(course_id: rand(1..50), user_id: rand(1..15), enrolled: true, created: false) end
 
-# 30.times do Enrollment.create(course_id: rand(1..50), user_id: rand(1..15), enrolled: false, created: true) end
+30.times do Enrollment.create(course_id: rand(1..50), user_id: rand(1..15), enrolled: false, created: true) end
 
-# puts 'seeded'
+puts 'seeded'
