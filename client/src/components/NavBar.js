9ab566a67020c31ea56c601b358a5d7273d6cc6c
Donte Ladatto <donte279@gmail.com>
2023-10-02 11:24:52 -0500

    conditional navbar rendering

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,21 +3,66 @@ import { NavLink } from 'react-router-dom'
 import { logOut } from '../features/sessionsSlice'
 import { useDispatch, useSelector } from 'react-redux'
 
-// TODO: User Profile NavLink?
-// TODO: Only render Create Course link if user is instructor
-// TODO: Render user profile & logout link instead of login/signup when there is a user
-// TODO: Different styling and position for login, signup, logout links; they shouldn't be a tab per se
 
 const NavBar = () => {
 
   const dispatch = useDispatch()
   const user = useSelector(state => state.sessions.currentUser)
+  const loggedIn = useSelector(state => state.sessions.loggedIn)
 
   const handleClick = () => {
     fetch('/api/logout', {
       method: "DELETE"
     })
     dispatch(logOut())
+  }
+
+  if (!loggedIn) {
+    return (
+      <nav id="navbar">
+        <NavLink
+          to="/"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/courses"
+          end
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+        >
+          Course Catalog
+        </NavLink>
+        <NavLink
+          to="/login"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+        >
+          Login
+        </NavLink>
+        <NavLink
+          to="/signup/student"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+        >
+          Student Signup
+        </NavLink>
+        <NavLink
+          to="/signup/instructor"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+        >
+          Instructor Signup
+        </NavLink>
+      </nav>
+    )
   }
 
   return (
@@ -39,38 +84,18 @@ const NavBar = () => {
       >
         Course Catalog
       </NavLink>
-      <NavLink
-        to="/courses/new"
-        className={({ isActive, isPending }) =>
-          isPending ? "pending" : isActive ? "active" : ""
-        }
-      >
-        Create Course
-      </NavLink>
-      <NavLink
-        to="/login"
-        className={({ isActive, isPending }) =>
-          isPending ? "pending" : isActive ? "active" : ""
-        }
-      >
-        Login
-      </NavLink>
-      <NavLink
-        to="/signup/student"
-        className={({ isActive, isPending }) =>
-          isPending ? "pending" : isActive ? "active" : ""
-        }
-      >
-        Student Signup
-      </NavLink>
-      <NavLink
-        to="/signup/instructor"
-        className={({ isActive, isPending }) =>
-          isPending ? "pending" : isActive ? "active" : ""
-        }
-      >
-        Instructor Signup
-      </NavLink>
+      {
+        user.instructor ?
+          <NavLink
+            to="/courses/new"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "active" : ""
+            }
+          >
+            Create Course
+          </NavLink> :
+          null
+      }
       <NavLink
         to="/profile"
         className={({ isActive, isPending }) =>
