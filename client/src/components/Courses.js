904b96ba79ae7973a6b82a7c4f65f651cfadf8d7
alex lieberman <alex.lieberman182@gmail.com>
2023-10-03 23:03:08 -0700

    Adding filter

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CourseCard from './CourseCard'
 import { useSelector } from 'react-redux'
+import { Filter } from './Filter'
 
 const Courses = () => {
   const courses = useSelector(state => state.courses.entities)
-  const displayCourses = courses.map(c => <CourseCard key={c.id} course={c} />)
+  const [filterValue, setFilterValue] = useState('')
+
+  const courseFilter = courses.filter(course => course.title.toLowerCase().includes(filterValue.toLowerCase()))
+  
+  const displayCourses = courseFilter.map(c => <CourseCard key={c.id} course={c} />)
+
   if (!courses[0]) {
     return (
       <h2>There are no courses yet!</h2>
@@ -13,6 +19,7 @@ const Courses = () => {
   return (
     <div>
       <h2>Here is a list of all available EqualEd courses. Click on one for more info!</h2>
+      <Filter setFilterValue={setFilterValue} />
       <div className="course-grid">
         {displayCourses}
       </div>
